810b4d0babab2021faa75b6c34f519b3114c00f9
Jakob Demler <demler@noxum.com>
2015-10-10 17:04:50 -0700

    Initial Commit and Project Scaffold

---
+++ Compiler/Compiler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompiledHandlebars.Compiler
+{
+  public static class Compiler
+  {
+    public static string Compile(string hbsTemplate, string solutionPath)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ CompilerTests/CompilerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CompiledHandlebars.Compiler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompiledHandlebars.Compiler.Tests
+{
+  [TestClass()]
+  public class CompilerTests
+  {
+    [TestMethod()]
+    public void CompileTest()
+    {
+      Assert.Fail();
+    }
+  }
+}
