1a8fe9b242d4a0cf1d0597f449353843470a9006
Jakob Demler <demler@noxum.com>
2015-10-20 00:33:36 -0700

    Introduced RuntimeUtils NuGet Package

---
+++ RuntimeUtils/RenderHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompiledHandlebars.RuntimeUtils
+{
+  public static class RenderHelper
+  {
+    public static bool IsTruthy(bool b)
+    {
+      return b;
+    }
+
+    public static bool IsTruthy(string s)
+    {
+      return !string.IsNullOrEmpty(s);
+    }
+
+    public static bool IsTruthy(object o)
+    {
+      return o != null;
+    }
+  }
+
+  public class CompiledHandlebarsTemplateAttribute : Attribute { }
+}
