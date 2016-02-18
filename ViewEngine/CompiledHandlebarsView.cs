b4f9b095e598a52657486a68937baabb1b1bb195
Jakob Demler <demler@noxum.com>
2016-02-18 17:00:12 +0100

    Added Basic MVC ViewEngine

---
+++ ViewEngine/CompiledHandlebarsView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CompiledHandlebars.ViewEngine
+{
+  public class CompiledHandlebarsView : IView
+  {
+    private readonly Type _templateType;    
+
+    public CompiledHandlebarsView(Type templateType)
+    {
+      _templateType = templateType;
+    }
+
+    public void Render(ViewContext viewContext, TextWriter writer)
+    {
+      MethodInfo renderMethod = _templateType.GetMethod("Render");      
+      string output = (string)renderMethod.Invoke(null, new object[1] { viewContext.ViewData?.Model });
+      writer.Write(output);
+
+    }  
+  }
+}
---
+++ ViewEngine/CompiledHandlebarsViewEngine.cs
@@ -0,0 +1,48 @@
+﻿using CompiledHandlebars.RuntimeUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CompiledHandlebars.ViewEngine
+{
+  public class CompiledHandlebarsViewEngine : VirtualPathProviderViewEngine
+  {
+    private Dictionary<string, Type> _mappings { get; set; }
+    public CompiledHandlebarsViewEngine(Assembly assembly)
+    {
+      foreach(var template in assembly.GetTypes().Where(x => x.GetCustomAttribute(typeof(CompiledHandlebarsTemplateAttribute), false)!=null))
+      {
+        _mappings.Add(GetVirtualPath(assembly, template), template);
+      }
+    }
+
+    /// <summary>
+    /// Determines the VirtualPath for a Template.
+    /// </summary>
+    /// <param name="assembly"></param>
+    /// <param name="template"></param>
+    /// <returns></returns>
+    private string GetVirtualPath(Assembly assembly, Type template)
+    {
+      return string.Concat("~/", template.Namespace.Substring(assembly.GetName().Name.Length + 1), "/", template.Name.Replace('.', '/'), ".hbs");
+    }
+
+    protected override IView CreatePartialView(ControllerContext controllerContext, string partialPath)
+    {
+      if (_mappings.ContainsKey(partialPath))
+        return new CompiledHandlebarsView(_mappings[partialPath]);
+      return null;
+    }
+
+    protected override IView CreateView(ControllerContext controllerContext, string viewPath, string masterPath)
+    {
+      if (_mappings.ContainsKey(viewPath))
+        return new CompiledHandlebarsView(_mappings[viewPath]);
+      return null;
+    }
+  }
+}
